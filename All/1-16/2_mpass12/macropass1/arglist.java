e83a56da721f179471b48873c70833cbe08c33e5
Yash Jadhav <105788471+yashjadhav0220@users.noreply.github.com>
2022-11-20 21:37:31 +0530

    Add files via upload

---
+++ All/1-16/2_mpass12/macropass1/arglist.java
@@ -0,0 +1,9 @@
+
+
+public class arglist {
+	String argname;
+	arglist(String argument) {
+		// TODO Auto-generated constructor stub
+		this.argname=argument;
+	}
+}
---
+++ All/1-16/2_mpass12/macropass1/mdt.java
@@ -0,0 +1,10 @@
+
+
+
+public class mdt {
+String stmnt;
+public mdt() {
+	// TODO Auto-generated constructor stub
+	stmnt="";
+}
+}
